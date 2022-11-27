f3c9ab8e0e1e0e425e6100bd9c97a09945e5ef8b
YaOn999 <lumous999@gmail.com>
2022-11-28 00:09:23 +0300

    random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,5 +1,12 @@
-﻿int numberA = 31;
-int numberB = 5;
-// Console.WriteLine(numberA + numberB);
+﻿// int numberA = 31;
+// int numberB = 5;
+/// Console.WriteLine(numberA + numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
